38746cc0d374f10f46758bd4789a7075952cce10
pluja <github_pluja@r3d.red>
2020-09-01 21:03:51 +0200

    Add cited tweets to user feeds

--- app/routes.py
+++ app/routes.py
@@ -553,7 +553,7 @@ def getFeed(urls):
                     for post in userFeed[:-1]:
                         date_time_str = post.find('span', attrs={'class':'tweet-date'}).find('a')['title'].replace(",","")
                         time = datetime.datetime.now() - datetime.datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S')
-                        if time.days >= 11:
+                        if time.days >=8:
                             continue
 
                         if post.find('div', attrs={'class':'pinned'}):
@@ -596,41 +596,59 @@ def getFeed(urls):
     return feedPosts
 
 def getPosts(account):
-    avatarPath = "img/avatars/{}.png".format(str(random.randint(1,12)))
-    posts = []
+    feedPosts = []
         
     #Gather profile info.
-    rssFeed = feedparser.parse('{instance}{user}/rss'.format(instance=NITTERINSTANCE, user=account))
-    #Gather posts
-    if rssFeed.entries != []:
-        for post in rssFeed.entries:
-            newPost = twitterPost()
-            newPost.username = rssFeed.feed.title.split("/")[1].replace(" ", "")
-            newPost.twitterName = rssFeed.feed.title.split("/")[0]
-            newPost.date = getTimeDiff(post.published_parsed)
-            newPost.timeStamp = datetime.datetime(*post.published_parsed[:6])
-            newPost.op = post.author
-            try:
-                newPost.userProfilePic = rssFeed.channel.image.url
-            except:
-                newPost.profilePicture = ""
-            newPost.url = post.link
-            newPost.content = Markup(post.description)
-            
-            if "Pinned" in post.title.split(":")[0]:
-                newPost.isPinned = True
+    rssFeed = urllib.request.urlopen('{instance}{user}'.format(instance=NITTERINSTANCE, user=account)).read()
+    #Gather feedPosts
+    res = rssFeed.decode('utf-8')
+    html = BeautifulSoup(res, "html.parser")
+    userFeed = html.find_all('div', attrs={'class':'timeline-item'})
+    if userFeed != []:
+            for post in userFeed[:-1]:
+                date_time_str = post.find('span', attrs={'class':'tweet-date'}).find('a')['title'].replace(",","")
+                time = datetime.datetime.now() - datetime.datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S')
+                if time.days >=8:
+                    continue
 
-            if "RT by" in post.title:
-                newPost.isRT = True
-                newPost.profilePic = ""
-            else:
-                newPost.isRT = False
-                try:
-                    newPost.profilePic = rssFeed.channel.image.url
-                except:
-                    newPost.profilePic = avatarPath
-            posts.append(newPost)
-    return posts
+                if post.find('div', attrs={'class':'pinned'}):
+                    if post.find('div', attrs={'class':'pinned'}).find('span', attrs={'icon-pin'}):
+                        continue
+
+                newPost = twitterPost()
+                newPost.op = post.find('a', attrs={'class':'username'}).text
+                newPost.twitterName = post.find('a', attrs={'class':'fullname'}).text
+                newPost.timeStamp = datetime.datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S')
+                newPost.date = post.find('span', attrs={'class':'tweet-date'}).find('a').text
+                newPost.content = Markup(post.find('div',  attrs={'class':'tweet-content'}))
+                
+                if post.find('div', attrs={'class':'retweet-header'}):
+                    newPost.username = post.find('div', attrs={'class':'retweet-header'}).find('div', attrs={'class':'icon-container'}).text
+                    newPost.isRT = True
+                else:
+                    newPost.username = newPost.op
+                    newPost.isRT = False
+                
+                newPost.profilePic = NITTERINSTANCE+post.find('a', attrs={'class':'tweet-avatar'}).find('img')['src'][1:]
+                newPost.url = NITTERINSTANCE + post.find('a', attrs={'class':'tweet-link'})['href'][1:]
+                if post.find('div', attrs={'class':'quote'}):
+                    newPost.isReply = True
+                    quote = post.find('div', attrs={'class':'quote'})
+                    if quote.find('div',  attrs={'class':'quote-text'}):
+                        newPost.replyingTweetContent = Markup(quote.find('div',  attrs={'class':'quote-text'}))
+                        
+                    if quote.find('a', attrs={'class':'still-image'}):
+                        newPost.replyAttachedImg = NITTERINSTANCE+quote.find('a', attrs={'class':'still-image'})['href'][1:]
+                    
+                    newPost.replyingUser=quote.find('a',  attrs={'class':'username'}).text
+                    post.find('div', attrs={'class':'quote'}).decompose()
+
+                if post.find('div',  attrs={'class':'attachments'}):
+                    if not post.find(class_='quote'):
+                        if  post.find('div',  attrs={'class':'attachments'}).find('a', attrs={'class':'still-image'}):
+                            newPost.attachedImg = NITTERINSTANCE + post.find('div',  attrs={'class':'attachments'}).find('a')['href'][1:]
+                feedPosts.append(newPost)
+    return feedPosts
 
 def getYoutubePosts(ids):
     videos = []
@@ -642,7 +660,7 @@ def getYoutubePosts(ids):
             for vid in rssFeed.entries:
                 time = datetime.datetime.now() - datetime.datetime(*vid.published_parsed[:6])
 
-                if time.days >= 15:
+                if time.days >=8:
                     continue
                 
                 video = ytPost()
