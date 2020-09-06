2dcae564d06780a2e49b93822f439c8581125c8a
pluja <github_pluja@r3d.red>
2020-09-06 12:11:05 +0200

    Move last_seen to index

--- app/models.py
+++ app/models.py
@@ -22,8 +22,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
