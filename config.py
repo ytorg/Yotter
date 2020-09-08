cd7fa8e23371a3f4e3534206cf3133005ed317f1
pluja <github_pluja@r3d.red>
2020-09-08 12:30:43 +0200

    Fix config.py.
    Default to .env DATABASE_URL, fallback to DATABASE_URL on app.db

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
