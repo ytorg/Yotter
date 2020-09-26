1c02914b90ab1b2ded73fd53d38b9d3faf3d0091
pluja <github_pluja@r3d.red>
2020-09-26 14:07:37 +0200

    Solves #81

--- app/routes.py
+++ app/routes.py
@@ -297,6 +297,20 @@ def channel(id):
     data = feedparser.parse(data.content)
 
     channelData = YoutubeSearch.channelInfo(id)
+
+    for video in channelData[1]:
+        print(video)
+        if config['nginxVideoStream']:
+            hostName = urllib.parse.urlparse(video['videoThumb']).netloc
+            video['videoThumb'] = video['videoThumb'].replace("https://{}".format(hostName), "").replace("hqdefault", "mqdefault")+"?host="+hostName
+        else:
+            video['videoThumb'] = video['videoThumb'].replace('/', '~')
+    if config['nginxVideoStream']:
+        hostName = urllib.parse.urlparse(channelData[0]['avatar']).netloc
+        channelData[0]['avatar'] = channelData[0]['avatar'].replace("https://{}".format(hostName), "")+"?host="+hostName
+    else:
+        channelData[0]['avatar'] = channelData[0]['avatar'].replace('/', '~')
+        
     return render_template('channel.html', form=form, btform=button_form, channel=channelData[0], videos=channelData[1], restricted=config['restrictPublicUsage'], config=config)
 
 @app.route('/watch', methods=['GET'])
