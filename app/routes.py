874d5eb28286760662be0b41677485fabc285252
pluja <github_pluja@r3d.red>
2020-10-02 13:53:46 +0200

    Changes variable name
    From  to

--- app/routes.py
+++ app/routes.py
@@ -100,7 +100,7 @@ def twitter(page=0):
 
     # Items range per page
     page_items = page*10
-    tenmore = page_items+16 
+    offset = page_items+16 
     # Pagination logic
     init_page = page-3
     if init_page < 0:
@@ -111,8 +111,8 @@ def twitter(page=0):
         total_pages = max_pages
 
     # Posts to render
-    if posts and len(posts) > tenmore:
-        posts = posts[page_items:tenmore]
+    if posts and len(posts) > offset:
+        posts = posts[page_items:offset]
     else:
         posts = posts[page_items:]
 
